e9a42dded3fda8b8e73c42a564d37bfe5e663512
Neha Dixit <neha.dx007@gmail.com>
2020-08-21 00:02:47 -0400

    Initial Commit

---
+++ FaceFinder_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceFinder_App
+//
+//  Created by Saurabh Dixit on 8/21/20.
+//  Copyright © 2020 Dixit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
